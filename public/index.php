a1b31d3c758b427eab88e23f051419adb2613d3f
sytnic <sytnic@list.ru>
2025-07-07 11:49:50 +0300

    8. Add page content

--- public/index.php
+++ public/index.php
@@ -8,6 +8,22 @@
   
   <div id="page">
 
+  <?php
+      // Show the homepage
+
+      // The homepage content could:
+      // * be static content (here or in a shared file)
+      // * show the first page from the nav
+      // * be in the database but add code to hide in the nav
+
+      // Содержимое главной страницы может быть:
+      // * статичным (здесь или в shared файле)
+      // * страница хранится в БД, показать первую страницу из навигационной системы,  
+      // * страница хранится в БД, но не относится к навигационной системе
+
+      include(SHARED_PATH . '/static_homepage.php');
+  ?>
+
   </div>
 </div>
 
